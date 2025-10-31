08d8173216e23c16881e14d78afb9305dfa27c51
YasmineX <79691154+Byasmine@users.noreply.github.com>
2025-10-31 17:38:56 +0100

    exercices 💯
    form 1 to 9

---
+++ Week1/Day1/ExerciceXP/exerciceXP.py
@@ -0,0 +1,69 @@
+
+# Execrice 1 :
+print("Execrice 1 :")
+
+print("Hello World \n"*5)
+
+# Execrice 2 :
+print("Execrice 2 :")
+
+print((99^3)* 8)
+
+# Execrice 3 :
+print("Execrice 3 :")
+
+#False
+#True
+#False
+#Error
+#False
+print (5 < 3)
+print (3 == 3)
+print (3 == "3")
+#print ("3" > 3)
+print ("Hello" == "hello")
+
+# Execrice 4 :
+print("Execrice 4 :")
+
+computer_brand = "Apple"
+print(f"I have an {computer_brand} computer")
+
+# Execrice 5 :
+print("Execrice 5 :")
+name = "yasmine"
+age = 23
+shoe_size = 38
+info = f"Hello, my name is {name}, I am {age} years old and my shoe size is {shoe_size}."
+print(info)
+
+# Execrice 6 :
+print("Execrice 6 :")
+a = 5
+b = 3
+if a > b:
+    print("Hello World")
+
+# Execrice 7 :
+print("Execrice 7 :")
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f"{num} is even")
+else:
+    print(f"{num} is odd")
+
+# Execrice 8 :
+print("Execrice 8 :")
+name2 = input("Enter your name: ")
+if name == name2.lower():
+    print("Yeey, "+name2+" We have the same name ^_^")
+else:
+    print("Nice to meet you, "+name2)
+
+# Execrice 9 :
+print("Execrice 9 :")
+height = float(input("Enter your height in cm: "))
+if height > 145:
+    print("You are tall enough to ride")
+else:
+    print("You are to short to ride")
