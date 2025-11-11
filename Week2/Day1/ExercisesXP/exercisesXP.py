56580ce2573390026b7b82d1e2fd3e27f2b4c263
YasmineX <79691154+Byasmine@users.noreply.github.com>
2025-11-11 13:50:21 +0100

    Week1_mini_project 🔢

---
+++ Week2/Day1/ExercisesXP/exercisesXP.py
@@ -0,0 +1,75 @@
+
+#Exercice 1:
+print("Exercise 1 : ")
+#  Define the Cat class
+class Cat:
+    def __init__(self, cat_name, cat_age):
+        self.name = cat_name
+        self.age = cat_age
+
+# Create three cat objects
+cat1 = Cat("Milo", 2)
+cat2 = Cat("Luna", 5)
+cat3 = Cat("Simba", 3)
+
+# Create a function to find the oldest cat
+def find_oldest_cat(cat1, cat2, cat3):
+    oldest = cat1
+    if cat2.age > oldest.age:
+        oldest = cat2
+    if cat3.age > oldest.age:
+        oldest = cat3
+    return oldest
+
+# Print the oldest cat's details
+oldest_cat = find_oldest_cat(cat1, cat2, cat3)
+print(f"The oldest cat is {oldest_cat.name}, and is {oldest_cat.age} years old.")
+
+#Exercice 2:
+print("Exercise 2 :")  # Dogs
+
+class Dog:
+    def __init__(self, name, height):
+        self.name = name
+        self.height = height
+
+    def bark(self):
+        print(f"{self.name} goes woof!")
+
+    def jump(self):
+        print(f"{self.name} jumps {self.height * 2} cm high!")
+
+#Create Dog Objects
+davids_dog = Dog("Rex", 50)
+sarahs_dog = Dog("Teacup", 20)
+
+# print Dog Details and Call Methods
+print(f"{davids_dog.name} is {davids_dog.height} cm tall.")
+davids_dog.bark()
+davids_dog.jump()
+
+print(f"{sarahs_dog.name} is {sarahs_dog.height} cm tall.")
+sarahs_dog.bark()
+sarahs_dog.jump()
+
+#Compare Dog Sizes
+if davids_dog.height > sarahs_dog.height:
+    print(f"{davids_dog.name} is taller.")
+else:
+    print(f"{sarahs_dog.name} is taller.")
+
+
+#Exercise 3 : 
+print (" Exercise 3 : ") #who's the song producer ?
+
+class Song:
+    def __init__(self,lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for item in self.lyrics:
+            print(item)
+
+    
+stairway = Song(["There’s a lady who's sure", "all that glitters is gold", "and she’s buying a stairway to heaven"])
+stairway.sing_me_a_song()
