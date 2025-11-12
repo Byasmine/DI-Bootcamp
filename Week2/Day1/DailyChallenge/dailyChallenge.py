1d2b40c2f695cf239692c2328d1b92f6bb98bf4c
YasmineX <79691154+Byasmine@users.noreply.github.com>
2025-11-12 21:05:40 +0100

    Week2_Day2_ExercicesXP 💯

--- Week2/Day1/DailyChallenge/dailyChallenge.py
+++ Week2/Day1/DailyChallenge/dailyChallenge.py
@@ -5,12 +5,19 @@ class Farm:
         self.farm_name = farm_name
         self.animals= {}
 
-    def add_animal (self,animal_type, count=1):
-        if animal_type in self.animals:
-            count+=1
-            self.animals[animal_type]= (count)
-        else:
-            self.animals[animal_type]= (count)
+    # def add_animal (self,animal_type, count=1):
+    #     if animal_type in self.animals:
+    #         count+=1
+    #         self.animals[animal_type]= (count)
+    #     else:
+    #         self.animals[animal_type]= (count)
+
+    def add_animal(self, **kwargs):
+         for animal_type, count in kwargs.items():
+            if animal_type in self.animals:
+                self.animals[animal_type] += count
+            else:
+                self.animals[animal_type] = count
 
     def get_info(self):
         header = f"{self.farm_name}'s farm : \n"
@@ -22,21 +29,32 @@ class Farm:
         return sorted(self.animals.keys())
     
     def get_short_info(self):
-        animal_list = list(self.animals.keys())
+        # Get sorted animal types
+        animal_types = self.get_animal_types()
 
-        if len(animal_list) == 1:
-            animals_str = animal_list[0]
+        # Add "s" if count > 1
+        pluralized = [
+            animal + "s" if self.animals[animal] > 1 else animal
+            for animal in animal_types
+        ]
+
+        # Format the list into a readable sentence
+        if len(pluralized) == 1:
+            animals_str = pluralized[0]
         else:
-            animals_str = ", ".join(animal_list[:-1]) + " and " + animal_list[-1]   
-        
+            animals_str = ", ".join(pluralized[:-1]) + " and " + pluralized[-1]
+
+        # Return final string
         return f"{self.farm_name}'s farm has {animals_str}."
 
+# macdonald = Farm("McDonald")
+# macdonald.add_animal('cow', 5)
+# macdonald.add_animal('sheep')
+# macdonald.add_animal('sheep')
+# macdonald.add_animal('goat', 12)
 
 macdonald = Farm("McDonald")
-macdonald.add_animal('cow', 5)
-macdonald.add_animal('sheep')
-macdonald.add_animal('sheep')
-macdonald.add_animal('goat', 12)
+macdonald.add_animal(cow=5, sheep=2, goat=12)
 
 print(macdonald.get_info())
 print(macdonald.get_animal_types())
